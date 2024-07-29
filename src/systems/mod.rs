9248c775cfaeaa62a8f7347495032c2f06575da1
philicxie <philicxie@163.com>
2024-07-30 00:39:42 +0800

    tooltip system

--- src/systems/mod.rs
+++ src/systems/mod.rs
@@ -6,6 +6,7 @@ mod random_move;
 mod end_turn;
 mod movements;
 mod hud;
+mod tooltips;
 
 use crate::prelude::*;
 
@@ -15,6 +16,7 @@ pub fn build_input_scheduler() -> Schedule {
         .flush()
         .add_system(map_render::map_render_system())
         .add_system(hud::hud_system())
+        .add_system(tooltips::tooltips_system())
         .add_system(entity_render::entity_render_system())
         .build()
 }
