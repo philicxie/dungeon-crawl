19d3a51cb8555d8053eb2f20581f401df59ce7ac
philicxie <philicxie@163.com>
2024-07-31 00:38:50 +0800

    chasing system

--- src/spawner.rs
+++ src/spawner.rs
@@ -31,7 +31,7 @@ pub fn spawn_monster(
             color: ColorPair::new(WHITE, BLACK),
             glyph,
         },
-        MovingRandomly{},
+        ChasingPlayer{},
         Health{current: hp, max: hp},
         Name(name)
     ));
