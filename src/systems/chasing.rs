8c64b62d16f425a33cafc7e9b73148c11aa206a0
philicxie <philicxie@163.com>
2024-08-05 02:04:25 +0800

    update monster fov

--- src/systems/chasing.rs
+++ src/systems/chasing.rs
@@ -5,6 +5,7 @@ use crate::systems::player_input::player_input;
 #[system]
 #[read_component(Point)]
 #[read_component(ChasingPlayer)]
+#[read_component(FieldOfView)]
 #[read_component(Health)]
 #[read_component(Player)]
 pub fn chasing(
@@ -12,7 +13,7 @@ pub fn chasing(
     ecs: &mut SubWorld,
     commands: &mut CommandBuffer
 ) {
-    let mut movers = <(Entity, &Point, &ChasingPlayer)>::query();
+    let mut movers = <(Entity, &Point, &ChasingPlayer, &FieldOfView)>::query();
     let mut positions = <(Entity, &Point, &Health)>::query();
     let mut players = <(&Point, &Player)>::query();
 
@@ -27,7 +28,10 @@ pub fn chasing(
         map,
         1024.0
     );
-    movers.iter(ecs).for_each(|(entity, pos, _)| {
+    movers.iter(ecs).for_each(|(entity, pos, _, fov)| {
+        if !fov.visible_tiles.contains(&player_pos) {
+            return;
+        }
         let idx = map_idx(pos.x, pos.y);
         if let Some(destination) = DijkstraMap::find_lowest_exit(&dijkstra_map, idx, map) {
             let distance = DistanceAlg::Pythagoras.distance2d(*pos, *player_pos);
