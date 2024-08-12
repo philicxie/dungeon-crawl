1e000687e9d48b0b3b07ab449784a758b8be7561
philicxie <philicxie@163.com>
2024-08-12 23:49:28 +0800

    level up system

--- src/map_builder/themes.rs
+++ src/map_builder/themes.rs
@@ -13,23 +13,25 @@ impl MapTheme for DungeonTheme {
         match tile_type {
             TileType::Floor => to_cp437('.'),
             TileType::Wall => to_cp437('#'),
+            TileType::Exit => to_cp437('>'),
         }
     }
 }
 
 pub struct ForestTheme {}
 
-impl ForestTheme {
-    pub fn new() -> Box<dyn MapTheme> {
-        Box::new(Self{})
-    }
-}
-
 impl MapTheme for ForestTheme {
     fn tile_to_render(&self, tile_type: TileType) -> FontCharType {
         match tile_type {
             TileType::Floor => to_cp437(';'),
             TileType::Wall => to_cp437('"'),
+            TileType::Exit => to_cp437('>'),
         }
+    }
+}
+
+impl ForestTheme {
+    pub fn new() -> Box<dyn MapTheme> {
+        Box::new(Self{})
     }
 }
--- src/systems/map_render.rs
+++ src/systems/map_render.rs
@@ -21,36 +21,23 @@ pub fn map_render(
             let offset = Point::new(camera.left_x, camera.top_y);
             let idx = map_idx(x, y);
             if map.in_bounds(pt) && (player_fov.visible_tiles.contains(&pt)
-                | map.revealed_tiles[idx]) {// (1)
-                let tint = if player_fov.visible_tiles.contains(&pt) {// (2)
+                | map.revealed_tiles[idx])
+            {
+                let tint = if player_fov.visible_tiles.contains(&pt) {
                     WHITE
                 } else {
                     DARK_GRAY
                 };
-                match map.tiles[idx] {
-                    TileType::Floor => {
-                        let glyph = theme.tile_to_render(map.tiles[idx]);
-                        draw_batch.set(
-                            pt - offset,
-                            ColorPair::new(
-                                tint, // (3)
-                                BLACK
-                            ),
-                            glyph
-                        );
-                    }
-                    TileType::Wall => {
-                        let glyph = theme.tile_to_render(map.tiles[idx]);
-                        draw_batch.set(
-                            pt - offset,
-                            ColorPair::new(
-                                tint,
-                                BLACK
-                            ),
-                            glyph
-                        );
-                    }
-                }
+
+                let glyph = theme.tile_to_render(map.tiles[idx]);
+                draw_batch.set(
+                    pt - offset,
+                    ColorPair::new(
+                        tint,
+                        BLACK
+                    ),
+                    glyph
+                );
             }
         }
     }
